98f2276fbd482ecac90519a58efd37dc3b4ffa33
Ivan Nedorizanyuk <ivannedorizanyk@gmail.com>
2025-01-16 11:21:18 +0200

    Used UnitOfWork pattern

---
+++ DAL/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using DAL.Repositories.Interfaces;
+
+namespace DAL.UnitOfWork;
+
+public interface IUnitOfWork : IDisposable
+{
+    public IRepository<TEntity> GetRepository<TEntity>(bool hasCustomRepository = false) where TEntity : class;
+    Task SaveChangesAsync();
+    IEmployeeRepository Employees { get; }
+    IIndicatorRepository Indicators { get;  }
+    IReportRepository Reports { get; }
+}
