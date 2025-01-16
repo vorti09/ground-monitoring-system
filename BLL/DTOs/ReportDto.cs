4f379ab4526cd6999ddc2e75fbc31a3c78e5d7e2
Ivan Nedorizanyuk <ivannedorizanyk@gmail.com>
2025-01-16 11:23:22 +0200

    Created data transfer objects

---
+++ BLL/DTOs/ReportDto.cs
@@ -0,0 +1,13 @@
+﻿using DAL.Enums;
+
+namespace BLL.DTOs;
+
+public class ReportDto
+{
+    public int Id { get; set; }
+    public int EmployeeId { get; set; }
+    public DateTime CreateDate { get; set; }
+    public string Content { get; set; }
+    public bool IsPrinted { get; set; }
+    public ReportStatus Status { get; set; }
+}
