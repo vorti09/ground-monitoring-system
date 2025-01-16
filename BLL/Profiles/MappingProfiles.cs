6a09d26ee774ee372fc3b9d2f38c4ee8def423c6
Ivan Nedorizanyuk <ivannedorizanyk@gmail.com>
2025-01-16 11:25:09 +0200

    Created mapping profiles

---
+++ BLL/Profiles/MappingProfiles.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using BLL.DTOs;
+using DAL.Entities;
+
+namespace BLL.Profiles;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<Employee, EmployeeDto>().ReverseMap();
+        CreateMap<Indicator, IndicatorDto>().ReverseMap();
+        CreateMap<Report, ReportDto>().ReverseMap();
+    }
+}
