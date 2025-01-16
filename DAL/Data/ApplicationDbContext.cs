364a0f7cebf7a454e3942fd21d9d57a740662710
Ivan Nedorizanyuk <ivannedorizanyk@gmail.com>
2025-01-16 11:20:50 +0200

    Added Database

---
+++ DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,39 @@
+﻿using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.Entity<Report>()
+            .HasMany(r => r.Indicators)
+            .WithOne()
+            .OnDelete(DeleteBehavior.Cascade);
+        
+        modelBuilder.Entity<Report>()
+            .HasOne(r => r.Employee)
+            .WithMany()
+            .HasForeignKey(r => r.EmployeeId)
+            .OnDelete(DeleteBehavior.Restrict);
+        
+        modelBuilder.Entity<Indicator>()
+            .Property(i => i.Name)
+            .IsRequired();
+
+        modelBuilder.Entity<Employee>()
+            .Property(e => e.Name)
+            .IsRequired();
+    }
+
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<Report> Reports { get; set; }
+    public DbSet<Indicator> Indicators { get; set; }
+}
---
+++ DAL/Data/EFUnitOfWork.cs
@@ -0,0 +1,101 @@
+﻿using DAL.Repositories.Impl;
+using DAL.Repositories.Impl.Base;
+using DAL.Repositories.Interfaces;
+using DAL.UnitOfWork;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace DAL.Data;
+
+public class EFUnitOfWork : IUnitOfWork
+{
+    private readonly ApplicationDbContext _context;
+    private Dictionary<Type, object> _repositories;
+    private bool disposed = false;
+    private EmployeeRepository? employeeRepository;
+    private IndicatorRepository? indicatorRepository;
+    private ReportRepository? reportRepository;
+
+    public EFUnitOfWork(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    
+    public IEmployeeRepository Employees
+    {
+        get
+        {
+            if (employeeRepository is null)
+                employeeRepository = new EmployeeRepository(_context);
+            return employeeRepository;
+        }
+    }
+    
+    public IIndicatorRepository Indicators
+    {
+        get
+        {
+            if (indicatorRepository is null)
+                indicatorRepository = new IndicatorRepository(_context);
+            return indicatorRepository;
+        }
+    }
+
+    public IReportRepository Reports
+    {
+        get
+        {
+            if (reportRepository is null)
+                reportRepository = new ReportRepository(_context);
+            return reportRepository;
+        }
+    }
+
+    public IRepository<TEntity> GetRepository<TEntity>(bool hasCustomRepository = false) where TEntity : class
+    {
+        if (_repositories == null)
+        {
+            _repositories = new Dictionary<Type, object>();
+        }
+
+        if (hasCustomRepository)
+        {
+            var customRepo = _context.GetService<IRepository<TEntity>>();
+            if (customRepo != null)
+            {
+                return customRepo;
+            }
+        }
+
+        var type = typeof(TEntity);
+        if (!_repositories.ContainsKey(type))
+        {
+            _repositories[type] = new BaseRepository<TEntity>(_context);
+        }
+
+        return (IRepository<TEntity>)_repositories[type];
+    }
+
+    public async Task SaveChangesAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+
+    public virtual void Dispose(bool disposing)
+    {
+        if (!disposed)
+        {
+            if (disposing)
+            {
+                _context.Dispose();
+            }
+        }
+
+        disposed = true;
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+}
